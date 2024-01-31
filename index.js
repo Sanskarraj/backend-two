fcd7bf1080149649a84ee710f82c6cf9e10b7d40
“[firstname <sanskarsrs9@gmail.com>
2024-01-31 15:58:50 +0530

    third

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.get('/qwert', (req, res) => {
     res.send('Hello qwwrt!')
 })
 
-app.get('/sex', (req, res) => {
-    res.send('Hello sex!')
+app.get('/s', (req, res) => {
+    res.send('Hello s!')
 })
 
 app.get('/xxx', (req, res) => {
@@ -22,6 +22,39 @@ app.get('/xxx', (req, res) => {
 app.get('/q', (req, res) => {
     res.send('<h1>i lovu</h1>')
 })
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'this is joke 1',
+            content: 'kuch bhi daal this is one pehle daal'
+        },
+        {
+            id: 2,
+            title: 'this is joke 2',
+            content: 'kuch bhi daal this is two pehle daal'
+        },
+        {
+            id: 3,
+            title: 'this is joke 3',
+            content: 'kuch bhi daal this is three pehle daal'
+        },
+        {
+            id: 4,
+            title: 'this is joke 4',
+            content: 'kuch bhi daal this is four pehle daal'
+        },
+        {
+            id: 5,
+            title: 'this is joke 5',
+            content: 'kuch bhi daal this is five pehle daal'
+        },
+
+    ];
+    res.send(jokes);
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
